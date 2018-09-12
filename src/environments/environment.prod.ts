9f0aac09471e13e87ec710f3c5215dae210f8720
elena-dragunova <33746898+elena-dragunova@users.noreply.github.com>
2018-09-12 20:47:21 +0500

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBZZQ1ENUFHbTG9LOmJlUdGVoGwpHMFsxc",
+    authDomain: "organicshop-5c1a1.firebaseapp.com",
+    databaseURL: "https://organicshop-5c1a1.firebaseio.com",
+    projectId: "organicshop-5c1a1",
+    storageBucket: "organicshop-5c1a1.appspot.com",
+    messagingSenderId: "760752046916"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBZZQ1ENUFHbTG9LOmJlUdGVoGwpHMFsxc",
+    authDomain: "organicshop-5c1a1.firebaseapp.com",
+    databaseURL: "https://organicshop-5c1a1.firebaseio.com",
+    projectId: "organicshop-5c1a1",
+    storageBucket: "organicshop-5c1a1.appspot.com",
+    messagingSenderId: "760752046916"
+  }
 };
