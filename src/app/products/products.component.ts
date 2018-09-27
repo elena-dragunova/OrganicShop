95563be341dfeededb6d51125367a2813918ad70
elena-dragunova <33746898+elena-dragunova@users.noreply.github.com>
2018-09-27 13:23:04 +0500

    Home Page with Catalog of Products with filtering through Categories

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../product.service";
+import {CategoryService} from "../category.service";
+import {ActivatedRoute} from "@angular/router";
+import {Product} from "../models/product";
+import 'rxjs/add/operator/switchMap';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+    products: Product[] = [];
+    filteredProducts: Product[];
+    categories$;
+    category: string;
 
-  constructor() { }
+    constructor(route: ActivatedRoute,
+                productService: ProductService,
+                categoryService: CategoryService) {
+        productService
+            .getAll()
+            .switchMap(products => {
+                this.products = products;
+                return route.queryParamMap;
+            })
 
-  ngOnInit() {
-  }
+            .subscribe(params => {
+                    this.category = params.get('category');
+                    this.filteredProducts = (this.category) ? this.products.filter(p => p.category === this.category) : this.products;
+                }
+            );
+        this.categories$ = categoryService.getAll();
+
+
+    }
+
 
 }
