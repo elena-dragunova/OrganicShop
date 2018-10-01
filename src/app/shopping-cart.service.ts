730267407a159db430f59407e87c1b4b7435c541
edragunova <elena-k86@mail.ru>
2018-10-01 17:37:09 +0500

    Shopping Cart Page was added and fields with products, quantities and price were made.

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
 import {AngularFireDatabase, FirebaseObjectObservable} from 'angularfire2/database';
-import {s} from '@angular/core/src/render3';
 import {Product} from './models/product';
 import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
 import {ShoppingCart} from './models/shopping-cart';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ShoppingCartService {
@@ -17,9 +18,10 @@ export class ShoppingCartService {
         });
     }
 
-    async getCart(): Promise<FirebaseObjectObservable<ShoppingCart>> {
+    async getCart(): Promise<Observable<ShoppingCart>> {
         let cartId = await this.getOrCreateCartId()
-        return this.db.object('/shopping-carts/' + cartId);
+        return this.db.object('/shopping-carts/' + cartId)
+            .map(x => new ShoppingCart(x.items));
     }
 
     private getItems(cartId: string, productId: string){
