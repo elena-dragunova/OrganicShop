de3f3b92596ff54b3f22ef71ed1b9da5383d212e
edragunova <elena-k86@mail.ru>
2018-10-03 17:15:23 +0500

    Changing quantity in the shopping cart was implemented, and clearing the cart as well.

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,11 +1,21 @@
 import {ShoppingCartItem} from './shopping-cart-item';
+import {Product} from './product';
 
 export class ShoppingCart {
     items: ShoppingCartItem[] = [];
     constructor(public itemsMap: {[productId: string]: ShoppingCartItem}) {
+        this.itemsMap = itemsMap || {};
         for (let productId in itemsMap ) {
             let item = itemsMap[productId];
-            this.items.push(new ShoppingCartItem(item.product, item.quantity));
+            /*let x = new ShoppingCartItem({
+                title: item.title,
+                imageUrl: item.imageUrl,
+                price: item.price,
+                $key: productId
+            });*/
+            /*Object.assign(x, item);
+            x.$key = productId;*/
+            this.items.push(new ShoppingCartItem({...item, $key: productId}));
         }
     }
 
@@ -23,5 +33,10 @@ export class ShoppingCart {
             count += this.itemsMap[productId].quantity;
         }
         return count;
+    }
+
+    getQuantity(product: Product) {
+        let item = this.itemsMap[product.$key];
+        return item ? item.quantity : 0;
     }
 }
